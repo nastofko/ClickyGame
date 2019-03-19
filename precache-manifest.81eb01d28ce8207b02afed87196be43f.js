d85995d0ee54be4bb6698c1661094dc2b35d163b
unknown <nastofko@gmail.com>
2019-03-18 21:41:54 -0700

    Updates

---
+++ precache-manifest.81eb01d28ce8207b02afed87196be43f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "aad543bcff8b307aaac3",
+    "url": "/ClickyGame/static/js/main.40f3c870.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/ClickyGame/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "aad543bcff8b307aaac3",
+    "url": "/ClickyGame/static/css/main.fc8c3fc7.chunk.css"
+  },
+  {
+    "revision": "0ea512201f27edcab9da1df876068472",
+    "url": "/ClickyGame/index.html"
+  }
+];
